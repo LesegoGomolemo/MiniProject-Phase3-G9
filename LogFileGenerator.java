7f40bcaef6919d6da6c53d8eafe978d0076cc408
khodanit <12626307+khodanit@users.noreply.github.com>
2019-03-14 03:03:13 +0200

    Update LogFileGenerator.java

--- LogFileGenerator.java
+++ LogFileGenerator.java
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package just;
-
 import java.util.Random;
-
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 /**
  *
  * @author khoda
@@ -14,6 +9,22 @@ import java.util.Random;
 public class LogFileGenerator {
     public static void main(String[] input)
     {
+        //Create new file
+        try {
+            //File object with desired name
+            File myObj = new File("Log_File.txt");
+            if (myObj.createNewFile()) {
+                
+            }
+            else {
+                System.out.println("File already exists.");
+            }
+        }
+        catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        
         Random rLogID = new Random();
         Random rHr = new Random();
         Random rMin = new Random();
@@ -30,38 +41,58 @@ public class LogFileGenerator {
         //Print log file:
         //For Random function:
         //int randomNumber = random.nextInt(max + 1 - min) + min;
+        String file = "";
         for(int i = 0;i<100;i++)
         {
             //Log ID
-            System.out.print("logID:" + String.format("%06d", i+1) );
+            file += "logID:" + String.format("%06d", i+1);
             //Date
-            System.out.print(", dateTime:" + (rDay.nextInt(31+1-1)+1) + "-" + (rMonth.nextInt(12+1-1)+1) + "-" + (rYear.nextInt(2018+1-2016)+2016));
+            file += ", dateTime:" + (rDay.nextInt(31+1-1)+1) + "-" + (rMonth.nextInt(12+1-1)+1) + "-" + (rYear.nextInt(2018+1-2016)+2016);
             //Time
-            System.out.print(" " + String.format("%02d",(rHr.nextInt(23+1-0)+0)) + ":" + String.format("%02d",(rMin.nextInt(59+1-0)+0)) + ":" + String.format("%02d",(rSec.nextInt(59+1-0)+0)));
+            file += " " + String.format("%02d",(rHr.nextInt(23+1-0)+0)) + ":" + String.format("%02d",(rMin.nextInt(59+1-0)+0)) + ":" + String.format("%02d",(rSec.nextInt(59+1-0)+0));
             //Device ID
-            System.out.print(", deviceID:" + String.format("%06d",(rDevice.nextInt(999999+1-111111)+111111)) );
+            file += ", deviceID:" + String.format("%06d",(rDevice.nextInt(999999+1-111111)+111111)) ;
             //User ID
-            System.out.print(", userID:" + String.format("%05d",(rDevice.nextInt(99999+1-11111)+11111)) );
+            file += ", userID:" + String.format("%05d",(rDevice.nextInt(99999+1-11111)+11111)) ;
             //Event
-            System.out.print(", event:");
+            file += ", event:";
             int event = rDevice.nextInt(3+1-1)+1;
-            switch(event){case 1: System.out.print("Withdrawal");break; case 2: System.out.print("Deposit");break;  case 3: System.out.print("Balance Enquiry");break; default: System.out.print("Unsuccessful");break;}
+            switch(event){
+                case 1: file += "Withdrawal";break; 
+                case 2: file += "Deposit";break;  
+                case 3: file += "Balance Enquiry";break; 
+                default: file += "Unsuccessful";break;
+            }
             
             //Authentication
-             System.out.print(", authenticationType:");
+             file += ", authenticationType:";
             int authentication = rAuthentication.nextInt(5+1-1)+1;
             switch(authentication)
             {
-                case 1: System.out.print("NFC");break;  
-                case 2: System.out.print("Pin");break;  
-                case 3: System.out.print("Facial Recognition");break;   
-                case 4: System.out.print("Finger Print");break;  
-                case 5: System.out.print("OTP");break;
-                case 6: System.out.print("Password");break;
-                default: System.out.print("Fail");break;
+                case 1: file += "NFC";break;  
+                case 2: file += "Pin";break;  
+                case 3: file += "Facial Recognition";break;   
+                case 4: file += "Finger Print";break;  
+                case 5: file += "OTP";break;
+                case 6: file += "Password";break;
+                default: file += "Fail";break;
             }
             
-            System.out.println("");
+            file += "\n";
+        }
+        
+        //Print log file
+        System.out.println(file);
+        
+        //Write to file
+        try {
+            FileWriter myWriter = new FileWriter("Log_File.txt");
+            myWriter.write(file);
+            myWriter.close();
+        }
+        catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
     }
 }
