440752adaa0aac863ab83e6597a09c8de7d3158a
KyleGaunt <48382034+KyleGaunt@users.noreply.github.com>
2019-03-15 10:39:46 +0200

    Final Heartbeat Files
    To be added to main system

--- Main.java
+++ Main.java
@@ -16,9 +16,9 @@ public class Main {
     public static double dispensed;
     public static double available;
     public static double capacity;
+    public static int faultyPercentage;
 
     public static void main(String[] args) {
-
 
       for(int i = 0; i < 20; i++)
         atms[i] = new ATM();
@@ -26,37 +26,37 @@ public class Main {
       for(int i = 0; i < 20; i++)
         if(atms[i].status.equals("Online")) working++;
 
+      title();
+
+      availability();
+
+      cash();
+
+      
+
+
+
+
+    }
+
+    public static void title() {
+      System.out.println(ANSI_WHITE + "**************************************************************************");
+      System.out.println("                    H E A R T B E A T   R E P O R T");
+      System.out.println("**************************************************************************");
       System.out.println();
 
-      printTemplate(1);
-
-      printStats();
-
-      printStatus(1);
-
-    }
-
-    public static void printTemplate(int x) {
-      if(x == 1) {
-        System.out.println(ANSI_WHITE + "******************************************************");
-        System.out.println("                 HEARTBEAT REPORT");
-        System.out.println("******************************************************");
-        System.out.println();
-      }
-    }
-
-    public static void printStats() {
       System.out.println("1. Statistics:");
       System.out.println();
+    }
 
+    public static void availability() {
       System.out.println("  1.1. Number of registered ATMs: "); // add ATM array size
       System.out.println();
       System.out.println("    1.1.1. Number of functional ATMs: "); // good ATMs
       System.out.println("    1.1.2. Number of faulty ATMs: "); // bad ATMs
       System.out.println();
 
-
-      int faultyPercentage = (20 - working) * 5;
+      faultyPercentage = (20 - working) * 5;
 
       System.out.print(ANSI_GREEN + "functional |");
       for(int i = 0; i < 50 - (faultyPercentage / 2); i++)
@@ -69,9 +69,12 @@ public class Main {
 
       System.out.print("| faulty\n");
       System.out.println();
+
       System.out.println(ANSI_WHITE + "    Functional ATM Percentage: " + ANSI_GREEN +(100 - faultyPercentage));
       System.out.println(ANSI_WHITE + "    Faulty ATM Percentage: " + ANSI_RED + faultyPercentage);
+    }
 
+    public static void cash() {
       for(int i = 0; i < 20; i++)
         dispensed += atms[i].withdrawals;
 
@@ -84,25 +87,4 @@ public class Main {
       System.out.println(ANSI_WHITE + "   1.2.1. Total Cash Dispensed: R" + dispensed); // sum of all withdrawals
       System.out.println(ANSI_WHITE + "   1.2.2. Total Cash Available: R" + available); // sum of all withdrawals
     }
-
-    public static void printStatus(int x) {
-      System.out.println();
-      if(x == 0) {
-        System.out.println("Printing logs for Offline ATMs: ");
-        System.out.println();
-        for(int i = 0; i < 20; i++) {
-          if(atms[i].status.equals("Offline"))
-            atms[i].printLogs();
-        }
-      }
-      else if(x == 1){
-        System.out.println("Printing logs for Online ATMs: ");
-        System.out.println();
-        for(int i = 0; i < 20; i++) {
-          if(atms[i].status.equals("Online"))
-            atms[i].printLogs();
-        }
-      }
-    }
-
 }
