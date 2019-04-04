9754791866371575cf9c6030159a8a4366ba0023
khodanit <12626307+khodanit@users.noreply.github.com>
2019-04-05 01:47:35 +0200

    Index Updated to reflect Logs

--- src/dashboard/index.php
+++ src/dashboard/index.php
@@ -5,8 +5,8 @@
     <?php include 'charts.php';?>
 </div>
 
-<div class="logs" id="logs" style="display: none">
-
+<div class="logs" id="logs" style="display:none">
+  <?php include 'Logs.php';?>
 </div>
 <!-- EXIT BODY -->
 <?php include 'footer.php'; ?>
